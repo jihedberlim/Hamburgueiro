1f228853c8dd3b65957154ac1e256cd44e57718d
Jihed Elmograbi Berlim <jihedberlim@gmail.com>
2024-03-14 10:49:30 -0300

    feat: inclusion of files and new images
    The JavaScript files were separated to keep the code organized, as well as the creation to insert the products into the cart.
    
    Also the image files were rendered to keep the same size so that it maintains a standard when styling.

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,3 +1,5 @@
+import { catalogo } from "./utilidades";
+
 function abrirCarrinho() {
     document.getElementById("carrinho").classList.add("right-[0px]");
     document.getElementById("carrinho").classList.remove("right-[-360px]");
@@ -14,4 +16,18 @@ export function inicializarCarrinho() {
 
     botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
     botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adicionarCarrinho(idProduto) {
+    const produto = catalogo.find(p => p.id === idProduto);
+    const containerProdutosCarrinho = document.getElementById('produtos-carrinho');
+    const cartaoProdutoCarrinho = `<article class="flex bg-slate-100 rounded-lg p-1 relative">
+    <button id="fechar-carrinho" class="absolute top-0 right-2"><i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i></button>
+    <img src="./assets/images/${produto.nomeArquivoImagem}" alt="Carrinho: ${produto.nome}" class="h-24 rounded-lg">
+    <div class="py-2">
+        <p class="text-slate-900 text-sm">${produto.nome}</p>
+        <p class="text-green-500 text-lg">R$${produto.preco}</p>
+    </div>
+</article>`;
+    containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
---
+++ src/utilidades.js
@@ -0,0 +1,14 @@
+export const catalogo = [
+    {
+        id: 1,
+        nome: 'Hamburguer Clássico',
+        preco: 25.00,
+        nomeArquivoImagem: "burger-1.jpg",
+    }, 
+    {
+        id: 2,
+        nome: 'Hamburguer Mexicano',
+        preco: 27.00,
+        nomeArquivoImagem: "burger-2.jpg",
+    },
+];
